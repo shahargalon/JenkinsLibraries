55107d2f7b511c4037008fa69dd305de7601e29e
Shahar Galon <shahar.galon@IL-TLV-MAC0119.local>
2021-10-04 10:25:50 +0300

    try with Map config

--- vars/ECR.groovy
+++ vars/ECR.groovy
@@ -1,4 +1,4 @@
-def ECRlogin_PUSH() {
+def ECRlogin_PUSH(Map config = [:]) {
     sh(label: 'ECR login and docker push', script:
          '''
          #!/bin/bash
@@ -6,15 +6,15 @@ def ECRlogin_PUSH() {
            echo "Authenticate with ECR"
             set +x # Don't echo credentials from the login command!
             echo "Building New ECR Image"
-            eval $(aws ecr get-login --region ${AWS_REGION} --no-include-email)
+            eval $(aws ecr get-login --region ${config.AWS_REGION} --no-include-email)
             # Enable Debug and Exit immediately 
             set -xe
-            echo ${GITCOMMIT}
-            docker build  -t ${config.IMAGE}:${GITCOMMIT} .
+            echo ${config.GITCOMMIT}
+            docker build  -t ${config.IMAGE}:${config.GITCOMMIT} .
             #two push one for master tag other is git commit ID
-            docker push ${IMAGE}:${GITCOMMIT}
-            docker tag ${IMAGE}:${GITCOMMIT} ${IMAGE}:latest
-            docker push ${IMAGE}:latest
+            docker push ${config.IMAGE}:${config.GITCOMMIT}
+            docker tag ${config.IMAGE}:${config.GITCOMMIT} ${config.IMAGE}:latest
+            docker push ${config.IMAGE}:latest
          '''.stripIndent())
 
       }
